85271b9aa15467d83e80f5f84bc445a28b352f23
Anuj Dubey <dubey96.anuj@gmail.com>
2024-11-24 14:13:52 +0000

    Added basic code for Matrix Multiplication using kernel

--- Matrix Multiplication/matrix_multiplication1.cu
+++ Matrix Multiplication/matrix_multiplication1.cu
@@ -5,11 +5,11 @@ __global__ void matrixMulKernel(float *A,float *B, float *C, int M, int N, int K
 {
   int row_id = blockDim.x * blockIdx.x + threadIdx.x;
   int col_id = blockDim.y * blockIdx.y + threadIdx.y;
-  int stride = blockDim.x *gridDim.x;
+  //int stride = blockDim.x *gridDim.x;
   
 
   if(row_id <M && col_id<N){
-      //printf("R,C,S: %d %d %d\n",row_id,col_id,stride);
+      //printf("R,C,S: %d %d\n",row_id,col_id);
 
       for(int k=0;k<N;k+=1)
       {
@@ -39,8 +39,8 @@ extern "C" void matrixMultiply(float *A, float *B, float *C, int M, int N, int K
     cudaMemcpy(A_gpu,A,size_a,cudaMemcpyHostToDevice);
     cudaMemcpy(B_gpu,B,size_b,cudaMemcpyHostToDevice);
     cudaMemcpy(C_gpu,C,size_c,cudaMemcpyHostToDevice);
-    dim3 blocks(1);
-    dim3 threads(4,4);
+    dim3 threads(32,32);
+    dim3 blocks((((M+threads.x-1)/threads.x) + 1), (((N+threads.y-1)/threads.y) + 1));
     //printf("Entered Here too!\n");
     matrixMulKernel<<<blocks, threads>>>(A_gpu,B_gpu,C_gpu,M,N,K);
 
